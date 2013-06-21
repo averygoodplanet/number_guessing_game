2b83292ac55a3e967cdb21079c8bba8244f2a483
Peter Himmelreich <averygoodplanet@gmail.com>
2013-06-21 00:32:44 -0500

    Added comparison between current and previous guess, e.g. 'You're getting warmer...'

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready( function () {
 	var min = 1;
 	var max = 10;
 	var repeat = true; //used in a do-while statement below to allow the user to choose to repeat the game.
+	var previous_guess = 0
 	
 	//getRandomInt function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 	function getRandomInt(min, max) {
@@ -10,6 +11,8 @@ $(document).ready( function () {
 	} 
 	
 	do {
+		var first_time_guess = 0; //keeping track of first time guessing, so won't compare "previous guess" to current guess on the first guess.
+		
 		var answer = getRandomInt(min, max);
 		
 		console.log("Answer: "+answer);
@@ -19,10 +22,23 @@ $(document).ready( function () {
 
 			console.log("Guess: "+guess);
 
+			var guess_difference = Math.abs(answer - guess);
+			var previous_guess_difference = Math.abs(answer - previous_guess);
+			var message = "";
+				
+			if (first_time_guess > 0) {
+				if (previous_guess_difference > guess_difference) {
+					message = "  You are getting warmer.";
+				}
+				if (previous_guess_difference < guess_difference) {
+					message = "  You are getting colder.";
+				}
+			}
+			
 			if (guess > answer) {
-				alert("Your guess was TOO HIGH.");
+				alert("Your guess was TOO HIGH."+message);
 			} else if (guess < answer) {
-				alert("Your guess was TOO LOW.");
+				alert("Your guess was TOO LOW."+message);
 			} else if (guess < min || guess > max) {
 				alert("Your guess was outside of the range of "+min+" to "+max);
 			}else if (guess == answer) {
@@ -31,6 +47,8 @@ $(document).ready( function () {
 			} else {
 				alert("I didn't understand your guess.");
 			}
+			previous_guess = guess;
+			first_time_guess += 1;
 		}
 		repeat = confirm("Press OK to play again.");
 	} while(repeat);
