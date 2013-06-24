27ffa1961bb44e03805a7547fa8bb96a18877f4c
Peter Himmelreich <averygoodplanet@gmail.com>
2013-06-23 23:58:40 -0500

    Starting html interface, need to look at getting number input next.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready( function () {
 	
 	var min = 1;
 	var max = 10;
-	var repeat = true; //used in a do-while statement below to allow the user to choose to repeat the game.
+	var game_on = false;
 	var previous_guess = 0
 	
 	//getRandomInt function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -10,19 +10,27 @@ $(document).ready( function () {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 	} 
 	
-	do {
+	$('button').on('click', function () { //Play button sets new hidden number.
+	
 		var first_time_guess = 0; //keeping track of first time guessing, so won't compare "previous guess" to current guess on the first guess.
 		
 		var answer = getRandomInt(min, max);
 		
 		console.log("Answer: "+answer);
 
-		while(true) {
-			var guess = prompt("I'm thinking of a number between "+min+" and "+max+" . Make a guess.");
+		game_on = true;	
+	});
+	
+	
 
+	/*$("#guess").keypress(function (event) {
+	
+		if ( event.which == 13) { //When ENTER pressed in guess box
+			
+			var guess = $("#play").val();
 			console.log("Guess: "+guess);
 
-			var guess_difference = Math.abs(answer - guess);
+			/*var guess_difference = Math.abs(answer - guess);
 			var previous_guess_difference = Math.abs(answer - previous_guess);
 			var message = "";
 				
@@ -42,14 +50,13 @@ $(document).ready( function () {
 			} else if (guess < min || guess > max) {
 				alert("Your guess was outside of the range of "+min+" to "+max);
 			}else if (guess == answer) {
-				alert("Your guess was correct.");
-				break;
+				alert("Your guess was correct. Press 'Play' to play again.");
 			} else {
 				alert("I didn't understand your guess.");
 			}
+			
 			previous_guess = guess;
-			first_time_guess += 1;
+			first_time_guess += 1; 
 		}
-		repeat = confirm("Press OK to play again.");
-	} while(repeat);
-});
+	}); */
+}); 
